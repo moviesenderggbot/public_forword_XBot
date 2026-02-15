d0457644fbde0c31b64fe0998b545afe518faa0b
xyzdevoloper <157599470+moviesenderggbot@users.noreply.github.com>
2026-02-15 22:02:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "f5bf404a0d7226894845c41b7e55cced")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8579539746:AAHQ1F2lYXU9BxTD564d9VH8R75X-gtqzUs") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://StarGiftBot_db_user:gld1RLm4eYbCWZlC@cluster0.erob6sp.mongodb.net/?appName=Cluster0)
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://StarGiftBot_db_user:gld1RLm4eYbCWZlC@cluster0.erob6sp.mongodb.net/?appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '7954726657').split()]
 
